77a49ed2c590b4e993028b6d371a4b6cc64e798d
alena <pylkaya@ya.ru>
2021-10-18 16:36:43 +0300

    feat: complete st-remove-from-list and st-stack tasks

--- src/st-remove-from-list.js
+++ src/st-remove-from-list.js
@@ -19,7 +19,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  */
 
-module.exports = function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function removeKFromList(l, k) {
+  let node = l;
+  if (!node) {
+    return;
+  }
+
+  while (node.next) {
+    if (node.value === k) {
+      node.value = node.next.value;
+      node.next = node.next.next;
+    }
+    node = node.next;
+  }
+  return l;
 }
